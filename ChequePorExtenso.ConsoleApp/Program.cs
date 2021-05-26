73d1f2d615fc8e256be8d6a69fb6ee7d6d9701eb
tiagosantini <tiagosantiniathayde@outlook.com>
2021-05-26 11:22:20 -0300

    Add interactive interface in Main()

--- ChequePorExtenso.ConsoleApp/Program.cs
+++ ChequePorExtenso.ConsoleApp/Program.cs
@@ -8,11 +8,54 @@ namespace ChequePorExtenso.ConsoleApp
     {
         public static void Main(string[] args)
         {
-            string strValorPorExtenso = Humanizador.EscreverPorExtenso(10.50m);
+            string strInput, resultado;
 
-            Console.WriteLine(strValorPorExtenso);
+            do
+            {
+                Console.Write("Digite o valor que deseja escrever por extenso: R$ ");
+
+                strInput = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(strInput))
+                {
+                    Console.Write("Digite algum valor! Pressione alguma tecla para tentar novamente... ");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+            } while (string.IsNullOrEmpty(strInput));
+
+
+            try
+            {
+                EscreverPorExtenso(strInput, out resultado);
+
+                ExibirResultado($"R$ {strInput} por extenso: {resultado}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             Console.ReadLine();
+        }
+
+        private static void ExibirResultado(string mensagem)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine(mensagem);
+
+            Console.ResetColor();
+        }
+
+        private static string EscreverPorExtenso(string strInput, out string resultado)
+        {
+            Decimal conversaoDecimal = Decimal.Parse(strInput);
+
+            resultado = Humanizador.EscreverPorExtenso(conversaoDecimal);
+
+            return resultado;
         }
     }
 }
